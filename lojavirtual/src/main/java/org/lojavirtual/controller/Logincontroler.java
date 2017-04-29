5d689851dea1d1bc044c4233fe5efdfa91e69f05
Caio Forte <caio.ti@live.com>
2017-04-29 19:16:56 +0000

    Algumas Alterçãoes na tel de login

--- lojavirtual/src/main/java/org/lojavirtual/controller/Logincontroler.java
+++ lojavirtual/src/main/java/org/lojavirtual/controller/Logincontroler.java
@@ -2,9 +2,11 @@ package org.lojavirtual.controller;
 
 import java.io.Serializable;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.lojavirtual.Login;
+import org.lojavirtual.service.Loginservice;
 
 @Named
 @javax.faces.view.ViewScoped
@@ -13,6 +15,19 @@ public class Logincontroler implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Login login;
+	@Inject
+	private Loginservice service;
 	
+	public void logar(Login login){
+		this.service.login(login);
+	}
+
+	public Login getLogin() {
+		return login;
+	}
+
+	public void setLogin(Login login) {
+		this.login = login;
+	}
 	
 }
